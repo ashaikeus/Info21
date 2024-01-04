94956a2b8a1782fce9c620edb4aaf21844a96f4a
vyperpunk <suloversanja@gmail.com>
2024-01-04 18:05:13 +0300

    part1.sql done, but path is absolute

--- src/part1.sql
+++ src/part1.sql
@@ -1,4 +1,4 @@
-CREATE TYPE check_state AS ENUM('Start', 'Success', 'Failure');
+CREATE TYPE check_state AS ENUM('0', '1', '2');
 CREATE TYPE in_or_out_state AS ENUM('1', '2');
 
 CREATE TABLE peers (
@@ -8,7 +8,7 @@ CREATE TABLE peers (
 
 CREATE TABLE tasks (
 	title VARCHAR(255) PRIMARY KEY,
-	parenttask VARCHAR(255) REFERENCES tasks(title),
+	parenttask VARCHAR(255),
 	maxXP BIGINT
 );
 
@@ -77,4 +77,25 @@ BEGIN
 END;
 $$;
 
-CALL from_csv('peers')
+CREATE OR REPLACE PROCEDURE to_csv(table_name_ VARCHAR(30), csv_separator VARCHAR(1) DEFAULT ';')
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+	csv_path VARCHAR = 'D:\SQL2_Info21_v1.0-2\src\CSV\' || table_name_ || '.csv';
+BEGIN
+	EXECUTE 'copy ' || table_name_ || ' to ''' || csv_path || ''' delimiter ''' || csv_separator || ''' csv header';
+END;
+$$;
+
+CALL from_csv('peers');
+CALL from_csv('tasks');
+CALL from_csv('checks');
+CALL from_csv('friends');
+CALL from_csv('p2p');
+CALL from_csv('recommendations');
+CALL from_csv('timetracking');
+CALL from_csv('transferredpoints');
+CALL from_csv('verter');
+CALL from_csv('xp');
+
+-- CALL to_csv('xp');
