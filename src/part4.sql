aa5d8823151c4d75d17f8ee5761e7329485eafc3
vyperpunk <suloversanja@gmail.com>
2024-01-20 14:43:16 +0300

    4.3 and 4.4 done, part 4 completed

--- src/part4.sql
+++ src/part4.sql
@@ -1,7 +1,7 @@
 -- 1)  Create a stored procedure that, without destroying the database, destroys all those tables
 -- in the current database whose names begin with the phrase 'TableName'.
-
-CREATE TABLE IF NOT EXISTS TableName0();
+-- создание тестовой таблицы для проверки:
+-- CREATE TABLE IF NOT EXISTS TableName0();
 
 CREATE OR REPLACE PROCEDURE drop_table_name() LANGUAGE PLPGSQL AS $$
 DECLARE current_table TEXT;
@@ -11,6 +11,7 @@ BEGIN
 	END LOOP;
 END;
 $$;
+-- для вызова:
 -- CALL drop_table_name();
 
 -----------------------------------------------------------------------------------------
@@ -37,6 +38,7 @@ BEGIN
 	END LOOP;
 END;
 $$;
+-- для вызова:
 -- DO $$
 -- DECLARE 
 --     function_number INT;
@@ -48,3 +50,55 @@ $$;
 
 -----------------------------------------------------------------------------------------
 
+-- 3) Create a stored procedure with output parameter, which destroys all SQL DML triggers
+-- in the current database. The output parameter returns the number of destroyed triggers.
+
+-- создание тестового триггера для проверки:
+-- CREATE OR REPLACE FUNCTION test_trigger_fun() RETURNS trigger AS $$ BEGIN RAISE NOTICE 'begin again'; END; $$ LANGUAGE plpgsql;
+-- CREATE OR REPLACE TRIGGER test_trigger BEFORE UPDATE ON peers FOR EACH ROW EXECUTE PROCEDURE test_trigger_fun();
+
+CREATE OR REPLACE PROCEDURE destroy_triggers(OUT trigger_number INT) LANGUAGE plpgsql AS $$
+DECLARE
+	current_trigger RECORD;
+BEGIN
+	trigger_number := 0;
+	FOR current_trigger IN SELECT *
+		FROM information_schema.triggers
+	LOOP
+		EXECUTE 'DROP TRIGGER IF EXISTS ' || current_trigger.trigger_name || ' ON ' || current_trigger.event_object_table;
+		trigger_number := trigger_number + 1;
+	END LOOP;
+END;
+$$;
+-- для вызова:
+-- DO $$
+-- DECLARE 
+--     trigger_number INT;
+-- BEGIN
+--     trigger_number := 0;
+--     CALL destroy_triggers(trigger_number);
+-- 	RAISE NOTICE 'trigger_number: %', trigger_number;
+-- END $$;
+
+-----------------------------------------------------------------------------------------
+
+-- 4) Create a stored procedure with an input parameter that outputs names and descriptions
+-- of object types (only stored procedures and scalar functions) that have a string
+-- specified by the procedure parameter.
+
+CREATE OR REPLACE PROCEDURE get_object_names_descriptions(IN string TEXT) LANGUAGE plpgsql AS $$
+DECLARE
+	current_function RECORD;
+BEGIN
+	FOR current_function IN SELECT *
+		FROM information_schema.routines
+		WHERE routine_type IN ('PROCEDURE', 'FUNCTION')
+		AND information_schema.routines.specific_schema = 'public'
+		AND information_schema.routines.routine_name LIKE '%' || string || '%'
+	LOOP
+		RAISE NOTICE '%', current_function.routine_name;
+	END LOOP;
+END;
+$$;
+-- для вызова:
+-- CALL get_object_names_descriptions('destroy');
